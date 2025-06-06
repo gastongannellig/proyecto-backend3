c5aa215949a588b4825baedefcaa9a8341d0805d
gaston gannelli <gastongannellig@hotmail.com>
2025-06-05 22:22:33 -0300

    PROYECTO FINAL BACKEND III NESTJS

--- src/modules/products/entities/product.entity.ts
+++ src/modules/products/entities/product.entity.ts
@@ -1,4 +1,3 @@
-// src/modules/products/entities/product.entity.ts
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
 import { Document } from 'mongoose';
 
--- src/modules/products/products.module.ts
+++ src/modules/products/products.module.ts
@@ -1,4 +1,3 @@
-// src/modules/products/products.module.ts
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { Product, ProductSchema } from './entities/product.entity';
